25d64e0fb3c6b4c3e3bb55482da32a55605f6b45
sea-shunned <cameron.shand@manchester.ac.uk>
2018-09-14 17:55:17 +0100

    Updated bash scripts

--- exp_centroid.sh
+++ exp_centroid.sh
@@ -1,5 +1,5 @@
-python run_mock.py --synthdata "*_9*" -m centroid --Lcomp 1 -e l1/centroid &&
-python run_mock.py --synthdata "*_9*" -m centroid --Lcomp 2 -e l2/centroid &&
-python run_mock.py --synthdata "*_9*" -m centroid --Lcomp 3 -e l3/centroid &&
-python run_mock.py --synthdata "*_9*" -m centroid --Lcomp 4 -e l4/centroid &&
-python run_mock.py --synthdata "*_9*" -m centroid --Lcomp 5 -e l5/centroid
+python run_mock.py --synthdata "*_9*" -m centroid --L_comp 1 -e l1/centroid &&
+python run_mock.py --synthdata "*_9*" -m centroid --L_comp 2 -e l2/centroid &&
+python run_mock.py --synthdata "*_9*" -m centroid --L_comp 3 -e l3/centroid &&
+python run_mock.py --synthdata "*_9*" -m centroid --L_comp 4 -e l4/centroid &&
+python run_mock.py --synthdata "*_9*" -m centroid --L_comp 5 -e l5/centroid
--- exp_neighour.sh
+++ exp_neighour.sh
@@ -1,5 +1,5 @@
-python run_mock.py --synthdata "*_9*" -m neighbour --Lcomp 1 -e l1/neighbour &&
-python run_mock.py --synthdata "*_9*" -m neighbour --Lcomp 2 -e l2/neighbour &&
-python run_mock.py --synthdata "*_9*" -m neighbour --Lcomp 3 -e l3/neighbour &&
-python run_mock.py --synthdata "*_9*" -m neighbour --Lcomp 4 -e l4/neighbour &&
-python run_mock.py --synthdata "*_9*" -m neighbour --Lcomp 5 -e l5/neighbour
+python run_mock.py --synthdata "*_9*" -m neighbour --L_comp 1 -e l1/neighbour &&
+python run_mock.py --synthdata "*_9*" -m neighbour --L_comp 2 -e l2/neighbour &&
+python run_mock.py --synthdata "*_9*" -m neighbour --L_comp 3 -e l3/neighbour &&
+python run_mock.py --synthdata "*_9*" -m neighbour --L_comp 4 -e l4/neighbour &&
+python run_mock.py --synthdata "*_9*" -m neighbour --L_comp 5 -e l5/neighbour
