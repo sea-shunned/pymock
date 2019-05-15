094e3d86ab5b554f1e09388813753e786b4db3c3
sea-shunned <cameron.shand@manchester.ac.uk>
2019-05-15 18:12:08 +0100

    Removed unnecessary einsum in VAR calc

--- objectives.py
+++ objectives.py
@@ -1,22 +1,18 @@
-import numpy as np
 import itertools
+
+import numpy as np
 from scipy.spatial.distance import cdist
 import igraph
-# from numba import jit
 
-def clusterChains(genotype, data_dict, comp_dict, reduced_clust_nums):
+def cluster_chains(genotype, data_dict, comp_dict, reduced_clust_nums):
     # Identify what base clusters the points in the new genotype are in
     new_clust_nums = [data_dict[i].base_cluster_num for i in genotype]
-
     # Create a graph
     g = igraph.Graph()
-    
     # Add each base cluster to the graph
     g.add_vertices(len(comp_dict))
-
     # Add the cluster merges to the graph as edges
-    g.add_edges(zip(reduced_clust_nums,new_clust_nums))
-    
+    g.add_edges(zip(reduced_clust_nums, new_clust_nums))
     # Find the connected components of this graph (the cluster chains)
     chains = list(g.components(mode="WEAK"))
 
@@ -24,14 +20,12 @@ def clusterChains(genotype, data_dict, comp_dict, reduced_clust_nums):
     #     genotype.num_clusts = len(chains)
     # except AttributeError:
     #     pass
-
     # Assign supercluster numbers to every member in each chain for easy membership check
     superclusts = np.empty(len(comp_dict), dtype=int)
     for i, chain in enumerate(chains):
         superclusts[chain] = i
     return chains, superclusts
 
-# @jit
 def objCNN(chains, superclusts, cnn_pairs, cnn_array, max_cnn):
     # Saves time for a single cluster solutions
     if len(chains) == 1:
@@ -51,41 +45,33 @@ def objVAR(chains, comp_dict, base_members, base_centres, superclusts):
     members = np.zeros((len(chains), 1))
     centres = np.zeros((len(chains), comp_dict[0].centroid.squeeze().shape[0]))
     variances = np.sum([obj.intraclust_var for obj in comp_dict.values()])
-    wcss_vec = np.zeros((len(chains),1))
-
     # Loop over all the chains/superclusters
     for superclust, chain in enumerate(chains):
-        centres[superclust, :] = np.sum(np.multiply(base_centres[chain], base_members[chain]), axis=0)
+        # Calculate the centroid for the supercluster
+        centres[superclust, :] = np.sum(
+            np.multiply(base_centres[chain], base_members[chain]),
+        axis=0)
+        # Get the number of members in each supercluster
         members[superclust] = np.sum(base_members[chain])
-
-        sub_res = base_centres[chain]-centres[superclust]
-
-        wcss_vec[superclust] = np.sum(np.multiply(base_members[chain], np.einsum('ij,ij->i', sub_res, sub_res)))
-
+    # We have taken the sum, so need to divide
     centres = np.divide(centres, members)
-
+    # Calculate the intracluster variance for the superclusters
     wcss = np.sum([comp_dict[index].num_members * np.dot(comp_dict[index].centroid.squeeze() - centres[value], comp_dict[index].centroid.squeeze() - centres[value]) for index, value in enumerate(superclusts)])
-    
     # print(wcss, variances)
     return wcss + variances
 
-
 # @profile
-def evalMOCK(genotype, comp_dict, reduced_clust_nums, cnn_array, max_cnn, num_examples, data_dict, cnn_pairs, base_members, base_centres):
+def eval_mock(genotype, comp_dict, reduced_clust_nums, cnn_array, max_cnn, num_examples, data_dict, cnn_pairs, base_members, base_centres):
     # Not really necessary but just in case
     if len(genotype) != len(reduced_clust_nums):
         raise ValueError("The genotype being evaluated is not the same length as the reduced, partial genotype")
-
     # Identifies which components are connected (delta-evaluation)
-    chains, superclusts = clusterChains(genotype, data_dict, comp_dict, reduced_clust_nums)
-
+    chains, superclusts = cluster_chains(genotype, data_dict, comp_dict, reduced_clust_nums)
     # Calculate the objectives
     CNN = objCNN(chains, superclusts, cnn_pairs, cnn_array, max_cnn)
     VAR = objVAR(chains, comp_dict, base_members, base_centres, superclusts)
-
     # Edge cases with large datasets, as single cluster solutions get given a 0 score
     if CNN < 0.00001: # Value that Garza/Handl uses, presumably to smooth small values
         CNN = 0
-
     # Divide by number of examples as per the paper and Mario's code
     return np.sum(VAR)/num_examples, CNN
