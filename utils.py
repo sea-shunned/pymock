b2fc9291ab6d1bf580e820b72262a1d6a98ddd5e
sea-shunned <cameron.shand@manchester.ac.uk>
2019-05-19 15:32:42 +0100

    Updated argparse to restrict usage to configs

--- utils.py
+++ utils.py
@@ -1,99 +1,31 @@
 import argparse
 import json
+from pathlib import Path
 
 def build_parser():
     parser = argparse.ArgumentParser()    
-
-    parser.add_argument(
+    # You need to either validate or give a config file name
+    group = parser.add_mutually_exclusive_group(require=True)
+    # Add the validate argument
+    group.add_argument(
         '-v', '--validate',
         help='validate current setup to ensure consistent results',
         action='store_true'
     )
-    parser.add_argument(
-        '-e', '--exp_name',
-        help='name of experiment for save location',
-        type=str,
-        default="",
-        metavar=''
+    # Add the config argument
+    group.add_argument(
+        '-c', '--config',
+        help='Name of the config file (must be in configs/ subdirectory)',
+        type=str
     )
-    parser.add_argument(
-        '-c', '--crossover',
-        help='crossover probability',
-        type=float,
-        default=1.0,
-        metavar=''
-    )
-    parser.add_argument(
-        '--synthdata',
-        help='string to specify what subset of synthetic data to use (* selects all)',
-        type=str,
-        default="*",
-        metavar=''
-    )
-    # parser.add_argument(
-    #     '--realdata',
-    #     help='string to specify what subset of real data to use (* selects all)',
-    #     type=str,
-    #     default="*"
-    # )
-    parser.add_argument(
-        '-m', '--mut_method',
-        help='specify which mutation method to use',
-        type=str,
-        default="original",
-        choices=["original", "centroid", "neighbour"],
-    )
-    parser.add_argument(
-        '-runs', '--num_runs',
-        help='specify the number of runs',
-        type=int,
-        default=30,
-        metavar=''
-    )
-    parser.add_argument(
-        '-gens', '--num_gens',
-        help='specify the number of generations',
-        type=int,
-        default=100,
-        metavar=''
-    )
-    parser.add_argument(
-        '-indivs', '--num_indivs',
-        help='specify the number of individuals in the population',
-        type=int,
-        default=100,
-        metavar=''
-    )
-    parser.add_argument(
-        '-L', '--L',
-        help='specify the neighbourhood parameter',
-        type=int,
-        default=10,
-        metavar=''
-    )
-    parser.add_argument(
-        '--L_comp',
-        help='specify the component neighbourhood parameter',
-        type=int,
-        default=3,
-        metavar=''
-    )
-    parser.add_argument(
-        '--seed_file',
-        help='specify the seed file for consistent experiments',
-        type=str,
-        default=None,
-        metavar=''
-    )    
     return parser
 
 def check_cl_args(cl_args):
-    if cl_args['validate'] and cl_args['exp_name']:
-        raise ValueError("Cannot set validate and an experiment name")
-    # This fails if user specifically gives default value
-    # It's fine
-    if cl_args['validate'] and cl_args['synthdata'] != "*":
-        raise ValueError("Validate uses only 1 dataset to check consistency")
+    # Create the path to the config file to check it
+    config_path = Path.cwd() / "configs" / cl_args["config"]
+    # Check that the config actually exists
+    if not config_path.is_file():
+        raise FileNotFoundError(f"Config file does not exist, expected {config_path}")
 
 def load_json(f_path="mock_config.json"):
     """Load config file for MOCK
